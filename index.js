5ca6fbe2a2558ebcbba2b49b8d58602940018d05
nic-d <nicanthonidiaz@gmail.com>
2023-11-10 11:47:14 -0500

    working on index file

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-const questions = [
+const cliQuestion = [
     {
-
-    },
-    {
-        
+        type: 'list',
+        name: 'initiate',
+        message: "Select one of the following:",
+        choices: ["View all departments","View all roles", "View all employees", "Add a department", "Add a role", "Add an employee", "Update an employee role"]
     }
-]
+];
+
