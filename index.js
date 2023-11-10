9097a1cf3c4ecd7ef6851024f4a35e97268a05ad
nic-d <nicanthonidiaz@gmail.com>
2023-11-10 11:08:03 -0500

    db created

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+const questions = [
+    {
+
+    },
+    {
+        
+    }
+]
---
+++ main/db/schema.sql
@@ -0,0 +1,37 @@
+DROP DATABASE IF EXISTS org_db;
+
+CREATE DATABASE org_db;
+
+USE org_db;
+
+-- department table --
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+-- role table --
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY(id)
+    FOREIGN KEY(department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+);
+
+-- employee table --
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NOT NULL,
+    PRIMARY KEY(id)
+    FOREIGN KEY(role_id) REFERENCES role(id)
+)
+
+-- SHOULD ALL THE ID VALUES BE AUTO_INCREMENTED IN EACH CASE HERE OR ISSUES CAN BE CAUSED?
